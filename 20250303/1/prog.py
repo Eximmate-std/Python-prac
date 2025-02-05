55f5446c66edb9dfb31b0c12b317615fb12b9eaa
Eximmate-std <inysh.av05@gmail.com.com>
2025-02-05 19:30:45 +0300

    In HW03 \1 added monster hp system

--- 20250303/1/prog.py
+++ 20250303/1/prog.py
@@ -5,38 +5,39 @@ player_position = (0, 0)
 game_map = [ (10 * [None]) for _ in range(10) ]
 
 class Entity:
-    def __init__(self, name, hello_word):
+    def __init__(self, name, hello_word, hp):
         self.hello_word = hello_word
         self.name = name
+        self.hp = hp
 
 
 def move(direction):
     global player_position
-    player_position = ((player_position[0] + direction[0] + 10) % 10, (player_position[1] + direction[1] + 10) % 10)
+    player_position = ((player_position[0] + direction[0] + 10) % 10,
+                       (player_position[1] + direction[1] + 10) % 10)
     print(f'Moved to ({player_position[0]}, {player_position[1]})')
-
     if game_map[player_position[1]][player_position[0]]:
         encounter()
-
     return player_position
 
 
-def add_monster(name, location, hello_word):
+def add_monster(name, hello_word, hp, location):
     if name not in cowsay.list_cows():
         print('Cannot add unknown monster')
         return
 
-    print(f'Added monster {name} to ({location[0]}, {location[1]}) saying {hello_word}')
+    x, y = location
+    print(f'Added monster {name} to ({x}, {y}) saying {hello_word}')
 
-    if game_map[player_position[1]][player_position[0]]:
+    if game_map[x][y]:
         print('Replaced the old monster')
 
-    game_map[location[1]][location[0]] = Entity(name, hello_word)
+    game_map[y][x] = Entity(name, hello_word, hp)
 
 
 def encounter():
-    print(cowsay.cowsay(game_map[player_position[1]][player_position[0]].hello_word,
-                        cow = game_map[player_position[1]][player_position[0]].name))
+    entity = game_map[player_position[1]][player_position[0]]
+    print(cowsay.cowsay(entity.hello_word, cow = entity.name))
 
 
 def main():
@@ -49,23 +50,22 @@ def main():
 
         try:
              match command:
-                case("up"):
+                case "up":
                     move((0, -1))
-                case("down"):
+                case "down":
                     move((0, 1))
-                case("right"):
+                case "right":
                     move((1, 0))
-                case("left"):
+                case "left":
                     move((-1, 0))
-                case("addmon"):
+                case "addmon":
                     if len(args) != 5:
                         raise ValueError
-                    add_monster(args[1], (int(args[2]), int(args[3])), args[4])
+                    add_monster(args[1], args[4], 100, (int(args[2]), int(args[3])))
                 case _:
                     print("Invalid command")
         except Exception as e:
             print('Invalid arguments')
-            print(e)
 
 
 if __name__ == "__main__":
